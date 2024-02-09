019ee30455d89416d59676ba28ede55ca1e2f948
mohamed khachlaa <97317412+meeedkhh@users.noreply.github.com>
2024-02-09 10:27:00 +0100

    Initial commit

---
+++ first/src/first/first.java
@@ -0,0 +1,28 @@
+package first;
+import java.util.Random;
+import java.util.*;
+import java.util.Scanner;
+
+public class first {
+	public static void main(String[] args) {
+	Scanner o = new Scanner(System.in);
+	System.out.println("donner un nombre");
+	int nbr = o.nextInt();
+	
+	int secret=10;
+	
+	while(nbr!=secret) {
+		if (nbr < secret){
+		System.out.println("donner un nombre plus grand");
+		nbr = o.nextInt();
+		}
+		else if (nbr > secret) {
+			System.out.println("donner un nombre plus petit");
+			nbr = o.nextInt();
+		}
+		else {
+			System.out.println("Bravo");
+		}
+	}
+}
+}
